1abd4a680188bb91c4b5ae5aab43a7c1711cf4cc
Aidan <14150272+Aidan-John@users.noreply.github.com>
2023-03-04 14:54:47 -0500

    update api logic

--- api/apiRouter.py
+++ api/apiRouter.py
@@ -19,50 +19,100 @@ def db_connect():
     ret = sqlite3.connect("users.db")
     return ret
 
-def init_db_table():
-    try:
-        db = db_connect()
-        db.execute("DROP TABLE users")
-        db.execute("CREATE TABLE users (user_id INTEGER PRIMARY KEY NOT NULL, name_first TEXT NOT NULL, name_last TEXT NOT NULL, points INTEGER NOT NULL);")
-        db.commit()
-        print("table initialized")
-    except:
-        print("something went wrong with table init")
-    finally:
-        db.close()
-
-def db_addUser(user):
-    try:
-        db = db_connect()
-        curr = db.cursor()
-        curr.execute("INSERT INTO users (user_id, name_first, name_last, points) VALUES (?,?,?,?)", (user['user_id'],user['name_first'],user['name_last'],user['points']))
-        db.commit()
-        print("User Added: ", user)
-    except:
-        print("something went wrong adding ", user)
-    finally:
-        db.close()
-
-def getAllUsers():
-    users = []
-    try:
-        db = db_connect()
-        db.row_factory = sqlite3.Row
-        curr = db.cursor()
-        curr.execute("SELECT * FROM users")
-        rows = curr.fetchall()
-
-        for user in rows:
-            userCopy = {}
-            userCopy['name_first'] = user['name_first']
-            userCopy['name_last'] = user['name_last']
-            userCopy['points'] = user['points']
-            userCopy['user_id'] = user['user_id']
-            users.append(userCopy)
-    except:
+class DB:
+    def __init__(self):
+        try:
+            db = db_connect()
+            db.execute("DROP TABLE users")
+            db.execute("CREATE TABLE users (user_id INTEGER PRIMARY KEY NOT NULL, name_first TEXT NOT NULL, name_last TEXT NOT NULL, points INTEGER NOT NULL);")
+            db.commit()
+            print("table initialized")
+        except:
+            print("something went wrong with table init")
+        finally:
+            db.close()
+    
+    def addUser(self, user):
+        try:
+            db = db_connect()
+            curr = db.cursor()
+            curr.execute("INSERT INTO users (user_id, name_first, name_last, points) VALUES (?,?,?,?)", (user['user_id'],user['name_first'],user['name_last'],user['points']))
+            db.commit()
+            print("User Added: ", user)
+        except:
+            print("something went wrong adding ", user)
+        finally:
+            db.close()
+    
+    def getAllUsers(self):
         users = []
-    finally:
-        return users
+        try:
+            db = db_connect()
+            db.row_factory = sqlite3.Row
+            curr = db.cursor()
+            curr.execute("SELECT * FROM users")
+            rows = curr.fetchall()
+
+            for user in rows:
+                userCopy = {}
+                userCopy['name_first'] = user['name_first']
+                userCopy['name_last'] = user['name_last']
+                userCopy['points'] = user['points']
+                userCopy['user_id'] = user['user_id']
+                users.append(userCopy)
+        except:
+            users = []
+        finally:
+            return users
+    
+    def search(self, user_id):
+        returnUser = {}
+        try:
+            db = db_connect()
+            db.row_factory = sqlite3.Row
+            curr = db.cursor()
+            curr.execute(f"SELECT * FROM users WHERE user_id='{user_id}'")
+            user = curr.fetchone()   
+            returnUser['name_first'] = user['name_first']
+            returnUser['name_last'] = user['name_last']
+            returnUser['points'] = user['points']
+            returnUser['user_id'] = user['user_id']
+        except:
+            returnUser = {}
+        finally:
+            return returnUser
+        
+    def delete(self, user_id):
+        try:
+            db = db_connect()
+            curr = db.cursor()
+            curr.execute(f"DELETE FROM users WHERE user_id='{user_id}'")
+            db.commit()
+            print(f"user #{user_id} has been deleted.")
+        except:
+            print(f"Something went wrong deleting user #{user_id}")
+
+    def getPoints(self, first, last):
+        points = 0
+        try:
+            db = db_connect()
+            db.row_factory = sqlite3.Row
+            curr = db.cursor()
+            curr.execute(f"SELECT points FROM users WHERE name_first='{first}' AND name_last='{last}'")
+            user = curr.fetchone()
+            points = int(user['points'])
+            
+        except:
+            points = 0
+        finally:
+            return points
+
+
+
+
+                
+
+
 
 
 
@@ -79,8 +129,32 @@ class userAction(Resource):
 
 ################# SETUP #################
     
-init_db_table()
+database = DB()
 
 for user in givenData:
-    db_addUser(user)
+    database.addUser(user)
+
+
+################# TESTING #################
+
+if __name__ == "__main__":
+    def prettyPrint(list):
+        for user in list:
+            print(user)
+
+    prettyPrint(database.getAllUsers())
+
+    newUser = {"name_first": "John", "name_last": "Doe", "user_id": 99, "points": 100}
+
+    database.addUser(newUser)
+
+    prettyPrint(database.getAllUsers())
+
+    print(database.search(newUser['user_id']))
+
+    print(database.getPoints("John", "Doe"))
+
+    database.delete(99)
+
+    prettyPrint(database.getAllUsers())
 
