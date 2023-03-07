1d847f72e3da44cc229c59520e0560d863bf20f2
Aidan <14150272+Aidan-John@users.noreply.github.com>
2023-03-07 16:18:35 -0500

    material ui integration, frontend cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,72 +1,245 @@
 import React, {useState, useEffect} from "react";
 
+import {Box, Fab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Menu} from '@mui/material'
 
-
-function AddUser(){
-  return (
-    <div className='Create-User'>
-      <form>
-        <label>User ID<input type='text'/></label>
-        <label>First Name<input type='text'/></label>
-        <label>Last Name<input type='text'/></label>
-        <label>Points<input type='text'/></label>
-      </form>
-    </div>
-  );
-}
-
-function SearchUser(){
-  return(
-    <div className="Search-User">
-      <form>
-        <label>First Name<input type='text' /></label>
-        <label>Last Name<input type='text' /></label>
-      </form>
-    </div>
-  )
-}
+import AddIcon from "@mui/icons-material/Add"
+import SearchIcon from "@mui/icons-material/Search"
+import DeleteIcon from "@mui/icons-material/Delete"
+import LoyaltyIcon from "@mui/icons-material/Loyalty"
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack"
+import Button from "@mui/material/Button"
 
 
 
+function ActionMenu(){
+  const [option, setOption] = useState(0);
 
 
-function InteractionMenu(){
-  const [menuOption, setMenuOption] = useState(0);
-
-  function Dropdown(){
+  function MenuChoices(){
     return(
-      <div class="dropdown">
-        <button class="dropbtn">Options</button>
-        <div class="dropdown-content">
-          <a onClick={ setMenuOption(1) }> Add User</a>
-          <a onClick={ setMenuOption(2) }> Search User</a>
-        </div>
-      </div>
-    )
+      <Box className="ActionMenu" sx={{ '& > :not(style)': { m: 1 } }} textAlign="center">
+        <Fab size="small" color="primary" aria-label="create" onClick={(e) => {e.preventDefault(); setOption(1);}}>
+          <AddIcon />
+        </Fab>
+        <Fab size="small" color="info" aria-label="search" onClick={(e) => {e.preventDefault(); setOption(2);}}>
+          <SearchIcon />
+        </Fab>
+        <Fab size="small" color="warning" aria-label="delete" onClick={(e) => {e.preventDefault(); setOption(3);}}>
+          <DeleteIcon />
+        </Fab>
+        <Fab size="small" color="secondary" aria-label="points" onClick={(e) => {e.preventDefault(); setOption(4);}}>
+          <LoyaltyIcon />
+        </Fab>
+      </Box>
+      
+    );
   }
+  function DeleteUser(){
+  
+    const [id, setID] = useState(0)
+    const [user, setUser] = useState({})
 
-  if (menuOption === 0){
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      if (id){
+        console.log(id)
+      }
+    }
+
     return(
-      <div className="Menu">
-        <Dropdown />
-      </div>
+      <Box className="ResultMenu" component="form" sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}} noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <Stack spacing={2} direction="column" align="center">
+          <TextField
+            label="User ID"
+            size="small"
+            onChange={(input) => setID(input.target.value)}
+            fullWidth
+            required
+          />
+
+          <Button sx={{maxWidth: '50%'}} variant="contained" color="warning" endIcon={<DeleteIcon />} type="submit">Delete</Button>
+        </Stack>
+      </Box>
+      
     )
-  }
-  if (menuOption === 1){
+
+
+  };
+  
+  function SearchUser(){
+  
+    const [id, setID] = useState(0)
+    const [user, setUser] = useState({})
+
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      if (id){
+        console.log(id)
+      }
+    }
+
+    return(
+      <Box className="ResultMenu" component="form" sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}} noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <Stack spacing={2} direction="column" align="center">
+          <TextField
+            label="User ID"
+            size="small"
+            onChange={(input) => setID(input.target.value)}
+            fullWidth
+            required
+          />
+
+          <Button sx={{maxWidth: '50%'}} variant="contained" endIcon={<SearchIcon />} type="submit">Search</Button>
+        </Stack>
+        
+      </Box>
+      
+    )
+
+
+  };
+
+  function AddUser(){
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [id, setID] = useState(0)
+    const [points, setPoints] = useState(0)
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      if (firstName){
+        console.log(firstName)
+      }
+    }
+
+    return(
+      <Box className="ResultMenu" component="form" sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}} noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <Stack spacing={2} direction="column" align="center">
+          <TextField
+            label="User ID"
+            size="small"
+            onChange={(input) => setID(input.target.value)}
+            fullWidth
+            required
+          />
+          <TextField
+            label="First Name"
+            size="small"
+            onChange={(input) => setFirstName(input.target.value)}
+            fullWidth
+            required
+          />
+          <TextField
+            label="Last Name"
+            size="small"
+            onChange={(input) => setLastName(input.target.value)}
+            fullWidth
+            required
+
+          />
+          <TextField
+            label="Points"
+            size="small"
+            onChange={(input) => setPoints(input.target.value)}
+            fullWidth
+            required
+          />
+          <Button sx={{maxWidth: '50%'}} variant="contained" endIcon={<AddIcon />} type="submit">Create</Button>
+
+
+            
+
+
+          
+          
+        </Stack>
+        
+      </Box>
+      
+    )
+
+
+  };
+
+  function PointsUser(){
+  
+    const [id, setID] = useState(0)
+    const [points, setPoints] = useState(0)
+
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      if (id){
+        console.log(id)
+      }
+    }
+
+    return(
+      <Box className="ResultMenu" component="form" sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}} noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <Stack spacing={2} direction="column" align="center">
+          <TextField
+            label="User ID"
+            size="small"
+            onChange={(input) => setID(input.target.value)}
+            fullWidth
+            required
+          />
+
+          <Button sx={{maxWidth: '50%'}} variant="contained" endIcon={<SearchIcon />} type="submit">Get Points</Button>
+        </Stack>
+        
+      </Box>
+      
+    )
+
+
+  };
+
+ 
+
+  if (option === 1){
     return (
-      <div className="Menu">
-        <Dropdown />
+      <div>
+        <MenuChoices />
         <AddUser />
+       
       </div>
     )
-  }else if (menuOption === 2){
+  } else if (option === 2){
     return (
-      <div className="Menu">
-        <Dropdown />
+      <div>
+        <MenuChoices />
         <SearchUser />
       </div>
     )
+  } else if (option === 3){
+    return (
+      <div>
+        <MenuChoices />
+        <DeleteUser />
+      </div>
+    )
+  } else if (option === 4){
+    return (
+      <div>
+        <MenuChoices />
+        <PointsUser />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <MenuChoices />
+      </div>
+    )
   }
+
+
+
+  
+
 
 }
 
@@ -84,34 +257,38 @@ function UserTable() {
 
 
   return (
-    <div className="UserTable">
-      <table>
-        <tbody>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Points</th>
-          </tr>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="userTable">
+        <TableHead>
+          <TableRow>
+            <TableCell>User ID</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Points</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {userData.map((val,key) => {
             return(
-              <tr key={key}>
-                <td>{val.user_id}</td>
-                <td>{val.name_first} {val.name_last}</td>
-                <td>{val.points}</td>
-              </tr>
+              <TableRow key={key}>
+                <TableCell component="th" scope="row">{val.user_id}</TableCell>
+                <TableCell align="right">{val.name_first} {val.name_last}</TableCell>
+                <TableCell align="right">{val.points}</TableCell>
+              </TableRow>
             )
           })}
-        </tbody>
-      </table>
-    </div>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
+
+
 
 function App(){
   return (
     <div className="App">  
       <UserTable />
-      <InteractionMenu />
+      <ActionMenu />
     </div>
   )
 }
